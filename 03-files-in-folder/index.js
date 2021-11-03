520116ef61ebbfacd2d71e424a8b6ebbb582d7a6
Diana26 <plohayadamochka@gmail.com>
2021-11-03 18:15:01 +0300

    feat 03 task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+
+ fs.readdir(path.join(__dirname, 'secret-folder'), (err, content) => {
+    if(err) {
+        console.log('This papka is not found');
+    }
+    else {
+        for (i =0; i < content.length; i++) {
+            let info = path.parse(content[i]);
+            fs.stat(path.join(__dirname, 'secret-folder', content[i]), (err, stats) =>{
+                if(stats.isFile()) {
+                console.log(`${info.name} - ${info.ext.slice(1)} - ${stats.size}kb`);
+                }
+            })
+        }
+    }
+})
+
