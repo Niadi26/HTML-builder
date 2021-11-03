e2e07afbd23255036690498ecfdd1535da834e46
Diana26 <plohayadamochka@gmail.com>
2021-11-03 22:59:52 +0300

    feat 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const { copyFile } = require('fs/promises');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) {console.log('Cant create directory')}
+});
+
+fs.readdir(path.join(__dirname, 'files-copy'), (err, copyItems) => {
+    if (err) {console.error};
+    for(x = 0; x < copyItems.length; x++) {
+        fs.unlink(path.join(__dirname, 'files-copy', copyItems[x]),()=>{});
+    }
+});
+
+fs.readdir(path.join(__dirname, 'files'), (err, items) => {
+    if (err) {
+        console.log('This papka is not found');
+    }
+    else {
+        for(i = 0; i < items.length; i++) {
+            fs.copyFile(path.join(__dirname, 'files', items[i]), path.join(__dirname, 'files-copy', items[i]), (err)=>{
+                if (err) {console.error}
+            });
+        }
+    }
+}); 
